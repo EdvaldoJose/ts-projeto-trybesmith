3b1a0a1c0772269033d40c1481802d76f6df85d2
Edvaldo Jose <edvaldojose003@gmail.com>
2023-02-08 02:59:51 -0300

    Requisitos 01-02

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import ProductService from '../services/product.service';
+
+class ProductController {
+  constructor(
+    private productService = new ProductService(),
+  ) { }
+
+  public create = async (req: Request, res: Response) => {
+    const product = req.body;
+
+    const productCreated = await this.productService.create(product);
+    res.status(201).json(productCreated);
+  };
+}
+
+export default ProductController;
