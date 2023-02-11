48117cc53c69850d7a42213a90934ade5eed72ad
Edvaldo Jose <edvaldojose003@gmail.com>
2023-02-11 11:35:47 -0300

    Atualizacao do test

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -9,10 +9,19 @@ export default class OrderModel {
   }
 
   public async getAll(): Promise<Order[]> {
-    const result = await this.connection.execute(`SELECT ord.id,
-    ord.user_id, JSON_ARRAYAGG(pro.id) as productsIds FROM Trybesmith
-      .orders as ord INNER JOIN Trybesmith.products as pro ON ord
-      .id = pro.orderId GROUP BY ord.id ORDER BY ord.user_id`);
+    const result = await this.connection.execute(
+      `SELECT orders.id, orders.user_id as userId, 
+    JSON_ARRAYAGG(products.id) AS 'productsIds'
+    FROM Trybesmith.orders
+    INNER JOIN Trybesmith.products
+    ON orders.id = products.order_id
+    GROUP BY orders.id`,
+    );
+      
+    //   `SELECT ord.id,
+    // ord.user_id, JSON_ARRAYAGG(pro.id) as productsIds FROM Trybesmith
+    //   .orders as ord INNER JOIN Trybesmith.products as pro ON
+    //   order_id = products, order_id GROUP BY ord.id ORDER BY ord.user_id`);
 
     const [rows] = result;
     return rows as Order[];
